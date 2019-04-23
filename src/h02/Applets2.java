db2d09e4f5965904881c5658634d277635db874c
SMJSGaming <sjoerd.t@live.nl>
2019-04-23 12:57:33 +0100

    deleted the comments

--- src/h02/Applets2.java
+++ src/h02/Applets2.java
@@ -1,25 +1,9 @@
 package h02;
 
-
-    /*
-
-    @(#)Show.java 1.0 03/02/26
-
-    *
-
-    Voorbeeld 2.1
-
-    *
-
-    */
-
-//importeer de benodigde klassen uit de Java bibliotheek
 import java.awt.*;
 import java.applet.*;
 
-//een klasse met de naam Show van het type Applet
 public class Applets2 extends Applet {
-    //een methode die de inhoud van het scherm tekent
     public void paint(Graphics g) {
         g.setColor(Color.blue);
         g.drawString("Sjoerd", 180, 60 );
